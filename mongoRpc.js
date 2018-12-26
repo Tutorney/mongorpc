806742b0e7215d7c726a03c118ca571bcb75c116
Doris Ortiz <373949-remainingduck@users.noreply.gitlab.com>
2018-12-26 12:15:21 +0800

    Make it possible to combine rpcs

--- mongoRpc.js
+++ mongoRpc.js
@@ -1,17 +1,31 @@
 const pRetry = require('p-retry');
 const {MongoClient} = require('mongodb');
 
+const call = async (url, {collection, dbName, method, args = []}) => {
+  console.log(collection, method, ...args);
+
+  const client = await pRetry(() => MongoClient.connect(url), {
+    maxRetryTime: 30000,
+  });
+  const db = client.db(dbName);
+  try {
+    const r = await db.collection(collection)[method](...args);
+    return typeof r.toArray === 'function' ? await r.toArray() : r;
+  } finally {
+    db.close();
+  }
+};
+
 module.exports = url =>
   async function mongoRpc(ctx) {
-    const {collection, dbName, method, args = []} = ctx.request.body;
-    console.log(collection, method, ...args);
-
-    const client = await pRetry(() => MongoClient.connect(url));
-    const db = client.db(dbName);
-    try {
-      const r = await db.collection(collection)[method](...args);
-      ctx.body = typeof r.toArray === 'function' ? await r.toArray() : r;
-    } finally {
-      db.close();
+    let body;
+    if (Array.isArray(ctx.request.body)) {
+      body = [];
+      for (const rpc of ctx.request.body) {
+        body.push(await call(url, rpc));
+      }
+    } else {
+      body = await call(url, ctx.request.body);
     }
+    ctx.body = body;
   };
